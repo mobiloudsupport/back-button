63acd961f97efafd58b841c9c692616aebb68503
tomigm <tgmoron@gmail.com>
2023-12-20 01:24:21 -0300

    Replace element added, code cleaning and docs

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ class BackButton {
     const defaultOptions = {
       time: '0.3s',
       textColor: 'white',
+      fillColor: 'white',
       buttonColorPrimary: '#000', // bg color
       buttonColorLight: '#fff', //not used, but could be used if darkMode is added
-      label: '←',
+      label: '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8 .4 34.3z"/></svg>',
       autoMatchOsTheme: true, //not used, but could be used if darkMode is added
       position: 'top-left', // top-right | top-left | bottom-left | bottom-right
-      animation: 'slideTop', // fadeIn | scaleUp | slideBottom | slideTop | slideleft | slideRight
-      display: 'onScrollDown', // onLoad | onScrollDown | onScrollUp
+      animation: null, // fadeIn | scaleUp | slideBottom | slideTop | slideleft | slideRight
+      display: 'onLoad', // onLoad | onScrollDown | onScrollUp
       hideMode: 'none', // not used for now
       scrollHeight: 300, //in px, not used now
       radius: '5px', // Any css unit, 50% gives a rounded btn if same height/width
-      width: '200px',
-      delay: '1000', // defines how much time to wait until the element shows up
+      width: '3em',
+      padding: '1em',
+      delay: '0', // defines how much time to wait until the element shows up
       backBehavior: 'javascript', // for now, javascript only
-      containerClass: null, // if not null, button is injected into a container, it uses any html selectors '.class' '#id' 
+      containerSelector: '.backBtn', // if not null, button is injected into a container, it uses any html selectors '.class' '#id' 
+      containerOrder: 'last', // 'inline-first', 'inline-last' | If container selector activated, it places the button before or after other elements
       textAlign: 'center', // center | start | end 
-      shadow: false
+      shadow: false,// If true applies soft shadow
+      replaceElement:  null // if a selector is placed here, this element would be replaced with the back button
     };
 
     options = Object.assign({}, defaultOptions, options);
@@ -58,9 +62,9 @@ class BackButton {
       .backButton-toggle {
         background: ${options.buttonColorPrimary};
         width: ${options.width};
-        height: 3rem;
-        padding: 1em;
-        position: ${options.containerClass == null ? 'fixed' : 'block' };
+        aspect-ratio: 1;
+        padding: ${options.padding};
+        position: ${options.containerSelector == null ? 'fixed' : 'inline-block' };
         border-radius: ${options.radius};
         border:none;
         right: ${positionValues.right};
@@ -74,6 +78,7 @@ class BackButton {
         align-items: center;
         z-index: 999999;
         color: ${options.textColor};
+        fill: ${options.fillColor};
         animation: ${options.animation + ' ' + '0.5s both'};
         box-shadow: ${options.shadow ? '0px 3px 15px rgba(0,0,0,0.2);' : 'none'}
         
@@ -88,7 +93,7 @@ class BackButton {
       }
 
       .backButton-toggle--visible {
-        display: flex
+        display: inline-flex
       }
 
     @keyframes fadeIn {
@@ -158,15 +163,6 @@ class BackButton {
     const button = document.createElement('button');
     button.innerHTML = options.label; //adds txt
 
-    // check if we would store history here
-    //const darkmodeActivated = window.localStorage.getItem('history') === '0';
-      if(options.containerClass  == null ) {
-        document.body.insertBefore(button, document.body.firstChild);
-      } else {
-        let container = document.querySelector(options.containerClass);
-        container.appendChild(button)
-      }
-
     // with 'this' refers to the fn addStyle below (1)
     this.addStyle(css);
 
@@ -177,6 +173,9 @@ class BackButton {
     this.scrollHeight = options.scrollHeight;
     this.delay = options.delay;
     this.backBehavior = options.backBehavior;
+    this.containerSelector = options.containerSelector;
+    this.containerOrder = options.containerOrder;
+    this.replaceElement = options.replaceElement;
   }
  // (1) inserts css in page
   addStyle(css) {
@@ -190,17 +189,37 @@ class BackButton {
 
 
   init() {
-    if (!IS_BROWSER) {
+    if (!IS_BROWSER || window.history.length <= 1 || window.location.pathname == '/') {
           return;
     }
-    const scrollHeight = this.scrollHeight   ;
     let backBehavior = this.backBehavior;
-    // keep, 'this' refers to the bubbotn in the constructor
+    const containerSelector = this.containerSelector;
+    const containerOrder = this.containerOrder;
     const button = this.button;
-    let scrollTop = 0;
-    // const hideMode = this.hideMode;
-    const display = this.display
-    
+    const display = this.display;
+    const replaceElement = this.replaceElement  
+    if(replaceElement) {
+      let element = document.querySelector(replaceElement)
+      element.replaceWith(button);
+
+    } 
+    else {
+      if(containerSelector  == null ) {
+        document.body.insertBefore(button, document.body.firstChild);
+      } else {
+        let container = document.querySelector(containerSelector);
+        switch (containerOrder) {
+          case 'first':
+            container.insertBefore(button, container.firstChild);
+            break;
+            case 'last':
+              container.appendChild(button);
+            break;
+          default:
+            container.appendChild(button)
+        } 
+      }
+    }
     const delay = this.delay
     const  displayMode = () => {
       let lastScrollTop = 0;
@@ -218,7 +237,7 @@ class BackButton {
             button.style.display = 'none';
           } else {
             // Scrolling up, show the element
-            button.style.display = 'flex';
+            button.style.display = 'inline-flex';
           }
   
           lastScrollTop = scrollTop;
@@ -234,7 +253,7 @@ class BackButton {
               button.style.display = 'none';
             } else {
               // Scrolling up, show the element
-              button.style.display = 'flex';
+              button.style.display = 'inline-flex';
             }
     
             lastScrollTop = scrollTop;
@@ -257,7 +276,7 @@ class BackButton {
     button.addEventListener('click', () => {
 
       // HERE GOES THE BACK FUNCTIONS
-      if(backBehavior === 'javascript') {
+      if(backBehavior === 'javascript' && window.history.length > 1) {
         history.back();
         console.log(history.length)
       }
